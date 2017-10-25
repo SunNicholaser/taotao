4fa0c8faf769b5ddf948816b16dff9ff5dfe7c0e
LiuXiaofeng <280404419@qq.com>
2017-10-25 22:49:39 +0800

    Initial commit

---
+++ taotao-manager-service/src/main/java/com/taotao/service/ItemService.java
@@ -0,0 +1,10 @@
+package com.taotao.service;
+
+import com.taotao.pojo.TbItem;
+
+/**
+ * Created by Administrator on 2017/10/25 0025.
+ */
+public interface ItemService {
+    TbItem getItemById(Long itemId);
+}
