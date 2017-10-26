f9b4f53bae3a722211a7709c9f8344d8cdcd5778
LiuXiaofeng <280404419@qq.com>
2017-10-26 21:08:12 +0800

    淘淘商城第二天

---
+++ taotao-manager-web/src/main/java/com/taotao/controller/PageController.java
@@ -0,0 +1,21 @@
+package com.taotao.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Administrator on 2017/10/26 0026.
+ * 页面跳转
+ */
+@Controller
+public class PageController {
+    @RequestMapping("/")
+    public String showIndex(){
+        return "index";
+    }
+    @RequestMapping("/{page}")
+    public String showPage(@PathVariable String page){
+        return page;
+    }
+}
---
+++ taotao-manager-web/src/test/java/com/taotao/pagehelper/TestPageHelper.java
@@ -0,0 +1,38 @@
+package com.taotao.pagehelper;
+
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.taotao.mapper.TbItemMapper;
+import com.taotao.pojo.TbItem;
+import com.taotao.pojo.TbItemExample;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2017/10/26 0026.
+ */
+public class TestPageHelper {
+    @Test
+    public void testPageHelper() throws Exception{
+        //获得mapper代理对象
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:spring/applicationContext-*.xml");
+        TbItemMapper itemMapper = applicationContext.getBean(TbItemMapper.class);
+
+        //设置分页
+        PageHelper.startPage(1,30);
+
+        //执行查询
+        TbItemExample example = new TbItemExample();
+        List<TbItem> list = itemMapper.selectByExample(example);
+        //获取分业后的结果
+        PageInfo<TbItem> pageInfo = new PageInfo<TbItem>(list);
+        long total = pageInfo.getTotal();
+        System.out.println("total:"+total);
+        int pages = pageInfo.getPages();
+        System.out.println("pages:"+pages);
+
+    }
+}
